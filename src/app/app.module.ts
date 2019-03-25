84179786162a69ca20add07c0e038b7bd546be8c
Fabian Vilers <fabian.vilers@dev-one.com>
2019-03-25 11:29:36 +0100

    Add Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { NgxFileHelpersModule } from 'ngx-file-helpers';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NgxFileHelpersModule],
+  imports: [BrowserModule, NgxFileHelpersModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
